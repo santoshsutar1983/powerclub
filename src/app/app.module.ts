41fa8fce8dc778869d0b798095010263fc53bcf9
Tejas Gawali <tejas.gawali@prisms.in>
2018-01-11 19:26:56 +0530

    cors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from "@angular/http";
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MembershipdetailsComponent } from './membershipdetails/membershipdetails.component';
@@ -11,10 +11,11 @@ import { HolidaysComponent } from './holidays/holidays.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    MembershipdetailsComponent,    
+    MembershipdetailsComponent,
     HolidaysComponent,
     NotificationsComponent,
     NotFoundComponentComponent
@@ -23,16 +24,17 @@ import { NotFoundComponentComponent } from './not-found-component/not-found-comp
     BrowserModule,
     HttpModule,
     RouterModule.forRoot([
-            { path : '',component : AppComponent },
-            { path : 'memberdetails/:uid/:sid',component : MembershipdetailsComponent },
-            { path : 'holidays/:uid/:sid',component : HolidaysComponent },
-            { path : 'notifications/:uid/:sid',component : NotificationsComponent },
-            { path : '**',component: NotFoundComponentComponent}
+            { path : '', component : AppComponent },
+            { path : 'memberdetails/:uid/:sid', component : MembershipdetailsComponent },
+            { path : 'holidays/:uid/:sid', component : HolidaysComponent },
+            { path : 'notifications/:uid/:sid', component : NotificationsComponent },
+            { path : '**', component: NotFoundComponentComponent}
 
           ])
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
- 
+export class AppModule {
+
+ }
